317880d92fb3e26ac4d20fc899e9e89485f023b6
Jasper Jongepier <jasper@visiecom.com>
2014-07-25 15:19:54 +0200

    Allow for single online event by capturing state. Todo: objectify it all using jQuery or some other. https://github.com/haroldiedema/joii also looks very cool

--- js/index.js
+++ js/index.js
@@ -1,27 +1,57 @@
-var app = {
+var app = 
+{    
+    state_online: null,
+    
     initialize: function()
     {
         this.bindEvents();
     },
     
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function() 
+    {
+        // Possible events: deviceready    pause    resume    backbutton    menubutton    searchbutton    startcallbutton    endcallbutton    volumedownbutton    volumeupbutton
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        
+        // org.apache.cordova.network-information: online offline
         document.addEventListener('online', this.onOnline, false);
+        document.addEventListener('onlineswitch', this.wentOnline, false);
         document.addEventListener('offline', this.onOffline, false);
+        document.addEventListener('offlineswitch', this.wentOffline, false);
+        
+        // org.apache.cordova.battery-status: batterycritical    batterylow    batterystatus
     },
-    
     onOnline: function()
     {
-        alert('online');
+        if(this.state_online === true)
+        {
+            return;
+        }
+        this.state_online = true;
+        var e = document.createEvent('Events'); 
+        e.initEvent("onlineswitch"); 
+        document.dispatchEvent(e);
     },
     onOffline: function()
     {
-        alert('offline');
+        if(this.state_online === false)
+        {
+            return;
+        }
+        this.state_online = false;
+        var e = document.createEvent('Events'); 
+        e.initEvent("offlineswitch"); 
+        document.dispatchEvent(e);
+    },
+    wentOnline: function()
+    {
+        alert('Online. Time to sync the data.');
+    },
+    wentOffline: function()
+    {
+        alert('Offline. When we have a certain version to use, we are cool. Notify the user about it.');
     },
     onDeviceReady: function() 
     {
-        alert('ready');
+        alert('Booted up.');
     },
 };
