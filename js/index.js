aeaf1231cbea3de8b2d07c499fa2555aeba6ce25
Jasper Jongepier <jasper@visiecom.com>
2014-07-29 15:01:32 +0200

    Client fixes

--- js/index.js
+++ js/index.js
@@ -1,57 +1,96 @@
-var app = 
-{    
+var app =
+{
     state_online: null,
-    
+    remote: 'http://192.168.1.123/zppc-server/api/json/read/app',
+    pagadata: null,
     initialize: function()
     {
-        this.bindEvents();
+        app.bindEvents();
     },
-    
-    bindEvents: function() 
+    bindEvents: function()
     {
         // Possible events: deviceready    pause    resume    backbutton    menubutton    searchbutton    startcallbutton    endcallbutton    volumedownbutton    volumeupbutton
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        
+        document.addEventListener('deviceready', app.initialized, false);
+
         // org.apache.cordova.network-information: online offline
-        document.addEventListener('online', this.onOnline, false);
-        document.addEventListener('onlineswitch', this.wentOnline, false);
-        document.addEventListener('offline', this.onOffline, false);
-        document.addEventListener('offlineswitch', this.wentOffline, false);
-        
+        document.addEventListener('online', app.onOnline, false);
+        document.addEventListener('onlineswitch', app.sync, false);
+        document.addEventListener('offline', app.onOffline, false);
+        document.addEventListener('offlineswitch', app.wentOffline, false);
+
         // org.apache.cordova.battery-status: batterycritical    batterylow    batterystatus
+    },
+    initialized: function()
+    {
+        // Do stuff.
     },
     onOnline: function()
     {
-        if(this.state_online === true)
+        if (app.state_online === true)
         {
             return;
         }
-        this.state_online = true;
-        var e = document.createEvent('Events'); 
-        e.initEvent("onlineswitch"); 
+        app.state_online = true;
+        var e = document.createEvent('Events');
+        e.initEvent("onlineswitch");
         document.dispatchEvent(e);
     },
     onOffline: function()
     {
-        if(this.state_online === false)
+        if (app.state_online === false)
         {
             return;
         }
-        this.state_online = false;
-        var e = document.createEvent('Events'); 
-        e.initEvent("offlineswitch"); 
+        app.state_online = false;
+        var e = document.createEvent('Events');
+        e.initEvent("offlineswitch");
         document.dispatchEvent(e);
-    },
-    wentOnline: function()
-    {
-        alert('Online. Time to sync the data.');
     },
     wentOffline: function()
     {
         alert('Offline. When we have a certain version to use, we are cool. Notify the user about it.');
     },
-    onDeviceReady: function() 
+    sync: function()
     {
-        alert('Booted up.');
+        if(!app.state_online)
+        {
+            return;
+        }
+        
+        //@todo: sync data, but only when new! and call app.utilizeData();
+        $.ajax
+        ({
+            accepts: "application/json",
+            beforeSend: function(x)
+            {
+                x.setRequestHeader("Content-Type","application/json");
+            },
+            url: app.remote, //@todo: add language here :)
+            dataType: 'json',
+            type: 'GET',
+            //@todo: error handling etc... make it beautiful
+            error: function(xhr,error,code) {
+                // SOMETHING WRONG WITH YOUR CALL.
+                  alert(error); 
+                  alert(xhr); 
+                  alert(code); 
+            },
+            success: function(data) 
+            {
+                //Store json.
+                app.utilizeData(data.data);
+            }
+        });
     },
+    utilizeData: function(data)
+    {
+        if(data.css)
+        {
+            $('#style_remote').remove();
+            $('head').append('<style type="text/css" id="style_remote">' + data.css + '</style>');
+        }
+        $('body').html(data.pagedata);
+        $.mobile.changePage('#home'); //@todo: the last remembered page :)
+        
+    }
 };
