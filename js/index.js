7e1960f5e5be945f0ec6660cc678911d1cd5ed24
Jasper Jongepier <jasper@visiecom.com>
2014-07-31 09:34:14 +0200

    alert test

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var app =
     cacheFile: null,
     initialize: function()
     {
-        console.log('Binding events...');
+        alert('Binding events...');
         app.bindEvents();
     },
     bindEvents: function()
@@ -30,22 +30,22 @@ var app =
     },
     initialized: function()
     {
-        console.log('Device ready!');
+        alert('Device ready!');
         app.ready = true;
         navigator.globalization.getLocaleName
         (
             function (locale) {app.lang = locale.value},
-            function () {console.log('Language could not be detected!');}
+            function () {alert('Language could not be detected!');}
         );
 
         app.whenReady();
     },
     onOnline: function()
     {
-        console.log('Device changed connection to online..');
+        alert('Device changed connection to online..');
         if (app.state_online === true)
         {
-            console.log('..but we already were online.');
+            alert('..but we already were online.');
             return;
         }
         app.state_online = true;
@@ -55,10 +55,10 @@ var app =
     },
     onOffline: function()
     {
-        console.log('Device changed connection to offline.');
+        alert('Device changed connection to offline.');
         if (app.state_online === false)
         {
-            console.log('..but we already were offline.');
+            alert('..but we already were offline.');
             return;
         }
         app.state_online = false;
@@ -82,7 +82,7 @@ var app =
     download: function(file_url, successFunction)
     {
         var parameters = {lang: app.lang};
-        console.log('Download file ' + file_url);
+        alert('Download file ' + file_url);
         $.ajax
         ({
             url: file_url,
@@ -98,7 +98,7 @@ var app =
     },
     updateAndOrInitializeData: function(path)
     {
-        console.log('Checking if we already have a cachefile..');
+        alert('Checking if we already have a cachefile..');
         if(app.cacheFile)
         {
             //@todo:might want to return now, and periodically check only...
@@ -109,7 +109,7 @@ var app =
             fileSystem.root.getFile(path, { create: false }, function(fileEntry)
             {
                 app.cacheFile = fileEntry.toURL();
-                console.log('.. we already have data at ' + app.cacheFile + ' . Checking if it is up to date before using it..');
+                alert('.. we already have data at ' + app.cacheFile + ' . Checking if it is up to date before using it..');
                 
                 app.download(app.cacheFile, function(data)
                 {
@@ -119,17 +119,17 @@ var app =
             function()
             {
                 app.cacheFile = false;
-                console.log('.. no data. Fetching data now.');
+                alert('.. no data. Fetching data now.');
                 if(!app.state_online)
                 {
-                    console.log('Not online (anymore). Cannot sync.');
+                    alert('Not online (anymore). Cannot sync.');
                     return;
                 }
                 app.update(app.remote + app.api_page, app.local_cachefile);
             });
         }, function(e)
         {
-           console.log('..error in checking if the file exists!' + e);
+           alert('..error in checking if the file exists!' + e);
         });
     },
     updateWhenNewVersion: function(remote_file, checksum)
@@ -147,22 +147,22 @@ var app =
             }
             else
             {
-                console.log('.. data is old (' + data.data + ' | ' + checksum + '). Fetching data now.');
+                alert('.. data is old (' + data.data + ' | ' + checksum + '). Fetching data now.');
                 app.update(app.remote + app.api_page, app.local_cachefile);
             }
         });
     },
     useCurrentData: function()
     {
-        console.log('..we are up to date!');
+        alert('..we are up to date!');
         if($('.app').hasClass('initializing'))
         {
-            console.log('Using the data we have..');
+            alert('Using the data we have..');
             app.utilizeFile(app.cacheFile);
         }
         else
         {
-            console.log('App is already loaded with latest data.');
+            alert('App is already loaded with latest data.');
         }  
     },
     update: function(remote_file, local_file)
@@ -182,12 +182,12 @@ var app =
                     local_path + local_file,
                     function(theFile) 
                     {
-                        console.log('Downloaded the latest version.');
+                        alert('Downloaded the latest version.');
                         app.utilizeFile(theFile.toURL());
                     },
                     function(error)
                     {
-                        console.log(error);
+                        alert(error);
                         return false;
                     },
                     {data: {lang: app.lang}}
