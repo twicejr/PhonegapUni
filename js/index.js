888ff490244cb6601ac88122a795f13f8b3e92d5
Jasper Jongepier <jasper@visiecom.com>
2014-07-31 10:11:16 +0200

    debug

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+if (typeof console  != "undefined") 
+    if (typeof console.log != 'undefined')
+        console.olog = console.log;
+    else
+        console.olog = function() {};
+
+console.log = function(message) {
+    console.olog(message);
+    $('.app').append('<p>' + message + '</p>');
+};
+console.error = console.debug = console.info =  console.log;
+
+
 var app =
 {
     fs: null,
