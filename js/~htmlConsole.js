6d42b4c9ff9dcc9429017ec6641c63aa2e256ff6
Jasper Jongepier <jasper@visiecom.com>
2014-09-03 12:48:54 +0200

    a

--- js/~htmlConsole.js
+++ js/~htmlConsole.js
@@ -7,6 +7,6 @@ if (typeof console  != "undefined")
 console.log = function(message) {
     console.olog(message);
     $('.loader').remove();
-    $('.app').append('<p>' + message + '</p>');
+    $('.app').prepend('<p>' + message + '</p>');
 };
 console.error = console.debug = console.info = console.log;
