3db16fcc54c9b33da95e99386d8373374676d1b4
Jasper Jongepier <jasper@visiecom.com>
2014-09-03 12:21:17 +0200

    ebug

--- js/~htmlConsole.js
+++ js/~htmlConsole.js
@@ -6,6 +6,6 @@ if (typeof console  != "undefined")
 
 console.log = function(message) {
     console.olog(message);
-    $('.app').append('<p>' + message + '</p>');
+    alert(message);
 };
 console.error = console.debug = console.info = console.log;
